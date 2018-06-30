da754dbf4ed265c2abe1d59d0a1a8cc81fe13326
tagrohan <40713164+tagrohan@users.noreply.github.com>
2018-06-30 18:00:52 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int * f(int a[])
+{
+    int i;
+    for(i=0;i<=9;i++)
+        a[i]=a[i]*a[i];
+    return(&a[0]);
+}
+void main()
+{
+    int i,a[10],*p;
+    for(i=0;i<=9;i++)
+    {
+        printf("Enter array element %d",i+1);
+        scanf("%d",&a[i]);
+    }
+    p=f(a);
+    printf("Array become\n");
+    for(i=0;i<=9;i++)
+     printf("%d ",*(p+i));
+    getch();
+
+}
